85f0f4797e9a01634c81cb904140998cef20e0aa
keep <1821893401@qq.com>
2020-12-09 11:13:47 +0800

    第二

---
+++ src/components/first.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg:"纪念"
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
